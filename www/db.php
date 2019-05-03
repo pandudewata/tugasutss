f0383934fc7c9fd62d152313a3cf6bd8e11c7578
ilhamp7 <48472174+ilhamp7@users.noreply.github.com>
2019-05-04 01:11:27 +0700

    Add files via upload

---
+++ www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id8404115_cordova","tugas","id8404115_tugascordova") or die ("could not connect database");
+?>
