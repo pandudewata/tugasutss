c4e1b512b7806addf12b6ab99ddd353f353d41ef
pandudewata <48473582+pandudewata@users.noreply.github.com>
2019-05-08 03:29:13 +0700

    Delete db.php

--- www/db.php
+++
@@ -1,4 +0,0 @@
-<?php
- header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","id8404115_cordova","tugas","id8404115_tugascordova") or die ("could not connect database");
-?>
