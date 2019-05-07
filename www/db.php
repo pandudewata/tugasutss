f8c3aabf73c2502858ecabfab30bca3cf1b3bb6b
pandudewata <48473582+pandudewata@users.noreply.github.com>
2019-05-08 03:31:07 +0700

    Add files via upload

---
+++ www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id8951475_pandu","pandu","id8951475_tugas7") or die ("could not connect database");
+?>
---
+++ www/update.php
@@ -0,0 +1,15 @@
+<?php
+ include "db.php";
+ if(isset($_POST['update']))
+ {
+ $id=$_POST['id'];
+ $kdliquid=$_POST['kdliquid'];
+ $nmliquid=$_POST['nmliquid'];
+ $harga=$_POST['harga'];
+ $q=mysqli_query($con,"UPDATE `vapestore` SET `kdliquid`='$kdliquid',`nmliquid`='$nmliquid',`harga`='$harga' where `id`='$id'");
+ if($q)
+ echo "success";
+ else
+ echo "error";
+ }
+ ?>
